75e1cafac4a7b579451cdadac142c79df1011078
Alexander Nerush <ale.nerush@gmail.com>
2016-01-30 09:12:31 +0200

    React Express(Handlebars) Webpack Babel

---
+++ client/components/app/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import './style.less'
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello from React!</div>
+    )
+  }
+}
+
+export default App
